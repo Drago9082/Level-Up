7f3252ed8b38034aa8ee3ac650a0ba8dc95e98c2
Odis Clemons <odis.clemons@gmail.com>
2022-06-08 12:36:02 -0400

    added global context

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import { AuthContext } from './context/Auth.js';
+import { AuthContext } from "./context/Auth.js";
+import globalContext from "./contexts/globalContext";
 
 //need this to set cookies
 axios.defaults.withCredentials = true;
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(undefined);
+  const [user, setUser] = userState({});
+
+  async function getLogged() {
+    const loggedResponse = await axios.get("/api/user/loggedIn");
+    setLoggedIn(loggedResponse.data);
+  }
+
+  useEffect(() => {
+    getLogged();
+  }, []);
+
   return (
     //We set the router in the authContext to provide context...
-    <AuthContext>
+    <globalContext.Provider value={{ loggedIn, setLoggedIn, user, setUser }}>
       <Router>
         <Nav />
         <Routes>
@@ -21,9 +34,8 @@ function App() {
           <Route path="*" element={<NoMatch />} />
         </Routes>
       </Router>
- </AuthContext>
+    </globalContext.Provider>
   );
 }
 
 export default App;
-
