53b336cf5356be46d8be171ebd7d7cfb0fb1f84d
DreadHeadDev <45825566+odisclemons@users.noreply.github.com>
2022-06-09 11:20:49 -0400

    Revert "pathing fixed"

--- client/src/pages/Presentation.js
+++ client/src/pages/Presentation.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 import Reveal from 'reveal.js';
 
-import 'reveal.js/dist/reset.css';
-import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/black.css';
+import '../../styles/reveal.js/reset.css';
+import '../../styles/reveal.js/reveal.css';
+import '../../styles/reveal.js/theme/black.css';
 
 export function Presentation() {
 
@@ -55,5 +55,3 @@ export function Presentation() {
 		</div>
     );
 }
-
-export default Presentation;
