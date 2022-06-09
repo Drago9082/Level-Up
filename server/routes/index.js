ec1145c6d000cffd7455c17a8f9a3b5dfdcd4f8d
Droessling94 <101940633+Droessling94@users.noreply.github.com>
2022-06-09 13:44:20 -0400

    plz

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const path = require('path')
 
 router.use('/api', apiRoutes);
-
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
