0c74819c27cad844327d8edbfccfef4d79372572
Droessling94 <101940633+Droessling94@users.noreply.github.com>
2022-06-09 13:22:16 -0400

    plz

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(
 );
 app.use(routes);
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 
